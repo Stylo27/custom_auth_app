0482c308a65e86655a6f701d712e8aff2ccd3b77
Stylo27 <styloff@gmail.com>
2018-01-16 18:12:00 +0200

    login without remember

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,13 @@
 class ApplicationController < ActionController::Base
-  include ApplicationHelper
-
   protect_from_forgery with: :exception
-  before_action :change_token
   helper_method :current_user
 
   def current_user
-    @current_user ||= User.find_by_auth_token(cookies[:auth_token]) if cookies[:auth_token]
+    @current_user = User.find_by_auth_token(cookies[:auth_token]) if cookies[:auth_token]
   end
 
   def authorize
     redirect_to root_path unless current_user
   end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      cookies.permanent[:auth_token] = user.auth_token
+      if params[:session][:remember_me] == "1"
+        cookies.permanent[:auth_token] = user.auth_token
+      else
+        cookies[:auth_token] = user.auth_token
+      end
       flash[:success] = 'You are signed'
       redirect_to root_path
     else
